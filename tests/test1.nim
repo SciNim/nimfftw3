2e7b638112d267f0c6de2ffe14aabf77433bfee0
Regis Caillaud <sidger@zoho.com>
2020-06-05 11:38:12 +0200

    Improved test

--- tests/test1.nim
+++ tests/test1.nim
@@ -13,47 +13,19 @@ import random
 import times
 import system
 
-proc fixConjugate*(output: Tensor[Complex64]): Tensor[Complex64] =
-  let
-    X = output.shape[0]
-    Y = output.shape[1]
-    Z = output.shape[2]
-    MZ = 2*Z - 1
-
-  result = newTensor[Complex64]([X, Y, MZ])
-  result[_, _, 0] = output[_, _, 0]
-
-  for x in 0||(X-1):
-    for y in 0||(Y-1):
-      for z in 1||((Z+1 div 2) - 1):
-        var mx = X - x
-        var my = Y - y
-        var mz = MZ - z
-
-        if x == 0:
-          mx = x
-
-        if y == 0:
-          my = y
-
-        result[mx, my, mz] = conjugate(output[x, y, z])
-        result[x, y, z] = output[x, y, z]
-
 proc mirrorTime[T](data: Tensor[T]): Tensor[T]=
   result = concat(data[_.._, _.._, ^1..1|-1], data, axis=2)
 
 proc main()=
-  let odims = @[4, 3, 7]
-  var dims: seq[int] = @[odims[0], odims[1], 2*odims[2]-1]
+  let dims = @[4, 3, 2*7-1]
 
-  var randData: Tensor[float64] = randomTensor(odims, 10).astype(float64)
+  var randData: Tensor[float64] = randomTensor(dims, 10).astype(float64)
 
   var r = initRand(epochTime().int64)
   apply_inline(randData):
     x + r.rand(100.0)/100.0
 
   ## Mirror data to avoid dealing with symetry in FFTW
-  randData = mirrorTime(randData)
 
   var output_dummy: Tensor[Complex64] = newTensor[Complex64](dims)
   var input_dummy: Tensor[Complex64] = newTensor[Complex64](dims)
