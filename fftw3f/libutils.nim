966175cdcf34d7fb2e182a515af6ca6a4aba6a84
Carlo Capocasa <carlo@capocasa.net>
2025-06-20 14:40:37 +0200

    fix prefix

--- fftw3f/libutils.nim
+++ fftw3f/libutils.nim
@@ -21,54 +21,54 @@ proc getFftw3Lib*() : string {.compiletime.}=
   return Fftw3Lib
 
 type
-  fftwfr2r_kind* = enum
+  fftwf_r2r_kind* = enum
     FFTW_R2HC = 0, FFTW_HC2R = 1, FFTW_DHT = 2, FFTW_REDFT00 = 3, FFTW_REDFT01 = 4, FFTW_REDFT10 = 5, FFTW_REDFT11 = 6, FFTW_RODFT00 = 7, FFTW_RODFT01 = 8, FFTW_RODFT10 = 9, FFTW_RODFT11 = 10
 
-  fftwfiodim* {.pure.} = object
+  fftwf_iodim* {.pure.} = object
     n*: cint
     `is`*: cint
     os*: cint
 
   ptrdiff_t* = clong
   wchar_t* = cint
-  fftwfiodim64* {.pure.} = object
+  fftwf_iodim64* {.pure.} = object
     n*: ptrdiff_t
     `is`*: ptrdiff_t
     os*: ptrdiff_t
 
-  fftwfwrite_char_func* = proc (c: char, a3: pointer) {.cdecl.}
-  fftwfread_char_func* = proc (a2: pointer): cint {.cdecl.}
+  fftwf_write_char_func* = proc (c: char, a3: pointer) {.cdecl.}
+  fftwf_read_char_func* = proc (a2: pointer): cint {.cdecl.}
   # Deprecated -> Use complex
-  fftwfcomplex = Complex64
-  fftwfplan* = pointer
+  fftwf_complex = Complex32
+  fftwf_plan* = pointer
 
 
-proc fftwffprint_plan*(p: fftwfplan, output_file: ptr FILE) {.cdecl, importc: "fftwffprint_plan", dynlib: Fftw3Lib.}
+proc fftwf_fprint_plan*(p: fftwf_plan, output_file: ptr FILE) {.cdecl, importc: "fftwf_fprint_plan", dynlib: Fftw3Lib.}
 
-proc fftwfprint_plan*(p: fftwfplan) {.cdecl, importc: "fftwfprint_plan", dynlib: Fftw3Lib.}
+proc fftwf_print_plan*(p: fftwf_plan) {.cdecl, importc: "fftwf_print_plan", dynlib: Fftw3Lib.}
 
-proc fftwfsprint_plan*(p: fftwfplan): cstring {.cdecl, importc: "fftwfsprint_plan", dynlib: Fftw3Lib.}
+proc fftwf_sprint_plan*(p: fftwf_plan): cstring {.cdecl, importc: "fftwf_sprint_plan", dynlib: Fftw3Lib.}
 
-proc fftwfmalloc*(n: csize_t): pointer {.cdecl, importc: "fftwfmalloc", dynlib: Fftw3Lib.}
+proc fftwf_malloc*(n: csize_t): pointer {.cdecl, importc: "fftwf_malloc", dynlib: Fftw3Lib.}
 
-proc fftwfalloc_real*(n: csize_t): ptr cdouble {.cdecl, importc: "fftwfalloc_real", dynlib: Fftw3Lib.}
+proc fftwf_alloc_real*(n: csize_t): ptr cdouble {.cdecl, importc: "fftwf_alloc_real", dynlib: Fftw3Lib.}
 
-proc fftwfalloc_complex*(n: csize_t): ptr fftwfcomplex {.cdecl, importc: "fftwfalloc_complex", dynlib: Fftw3Lib.}
+proc fftwf_alloc_complex*(n: csize_t): ptr fftwf_complex {.cdecl, importc: "fftwf_alloc_complex", dynlib: Fftw3Lib.}
 
-proc fftwffree*(p: pointer) {.cdecl, importc: "fftwffree", dynlib: Fftw3Lib.}
+proc fftwf_free*(p: pointer) {.cdecl, importc: "fftwf_free", dynlib: Fftw3Lib.}
 
-proc fftwfflops*(p: fftwfplan, add: ptr cdouble, mul: ptr cdouble, fmas: ptr cdouble) {.cdecl, importc: "fftwfflops",
+proc fftwf_flops*(p: fftwf_plan, add: ptr cdouble, mul: ptr cdouble, fmas: ptr cdouble) {.cdecl, importc: "fftwf_flops",
         dynlib: Fftw3Lib.}
 
-proc fftwfestimate_cost*(p: fftwfplan): cdouble {.cdecl, importc: "fftwfestimate_cost", dynlib: Fftw3Lib.}
+proc fftwf_estimate_cost*(p: fftwf_plan): cdouble {.cdecl, importc: "fftwf_estimate_cost", dynlib: Fftw3Lib.}
 
-proc fftwfcost*(p: fftwfplan): cdouble {.cdecl, importc: "fftwfcost", dynlib: Fftw3Lib.}
+proc fftwf_cost*(p: fftwf_plan): cdouble {.cdecl, importc: "fftwf_cost", dynlib: Fftw3Lib.}
 
-proc fftwfalignment_of*(p: ptr cdouble): cint {.cdecl, importc: "fftwfalignment_of", dynlib: Fftw3Lib.}
+proc fftwf_alignment_of*(p: ptr cdouble): cint {.cdecl, importc: "fftwf_alignment_of", dynlib: Fftw3Lib.}
 
-let fftwf_version* {.importc: "fftwf_version", dynlib: Fftw3Lib.}: cstring
+let fftwf_version* {.importc: "fftwf__version", dynlib: Fftw3Lib.}: cstring
 
-var fftwf_cc* {.importc: "fftwf_cc", dynlib: Fftw3Lib.}: cstring
+var fftwf_cc* {.importc: "fftwf__cc", dynlib: Fftw3Lib.}: cstring
 
-var fftwf_codelet_optim* {.importc: "fftwf_codelet_optim", dynlib: Fftw3Lib.}: cstring
+var fftwf_codelet_optim* {.importc: "fftwf__codelet_optim", dynlib: Fftw3Lib.}: cstring
 
